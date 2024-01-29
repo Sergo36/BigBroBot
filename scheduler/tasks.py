bde63041c70092eb4ac3fc1858da442d7c47a54f
RepinSS <repinsergeyvrn@gmail.com>
2024-01-29 20:53:10 +0300

    Everyday report fix
    Correct compare and message symbol limit

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 import aiogram.types
 from aiogram import Bot
 
@@ -28,11 +30,28 @@ async def everyday_report(notifier: TelegramNotifier):
              .select(Node.id.alias("node_id"), User.telegram_name.alias("username"), NodeType.name.alias("node_type"))
              .join(User, on=(User.id == Node.owner))
              .join(NodeType, on=(NodeType.id == Node.type))
-             .where((Node.obsolete == False) & (Node.expiry_date >= datetime.now().date()))
+             .where((Node.obsolete == False) & (Node.expiry_date <= datetime.now().date()))
              .namedtuples())
-    report_header = "Expiry date >= now\n"
+    data_len = 0
+    row_text = "Expiry date >= now\n"
+    row_len = len(row_text)
+    data_len = data_len + row_len
+
+    report_header = row_text
+
     report_data = []
     for row in query:
-        report_data.append(f"User: @{row.username}, Node: {row.node_type}, NodeId:{row.node_id}")
+        row_text = f"User: @{row.username}, Node: {row.node_type}, NodeId:{row.node_id}"
+        row_len = len(row_text)
+        data_len = data_len + row_len
+        if data_len < 4900:
+            report_data.append(f"User: @{row.username}, Node: {row.node_type}, NodeId:{row.node_id}")
+        else:
+            report = report_header + '\n'.join(report_data)
+            await notifier.emit("BigBroBot", report)
+            data_len = 0
+            row_text = "Expiry date >= now\n"
+            row_len = len(row_text)
+            data_len = data_len + row_len
     report = report_header + '\n'.join(report_data)
     await notifier.emit("BigBroBot", report)
