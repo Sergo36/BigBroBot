af0357a1f29a79b80aa494688367f44deb487a96
RepinSS <repinsergeyvrn@gmail.com>
2024-09-22 19:31:15 +0300

    Instalation result in one message

--- handlers/install/install.py
+++ handlers/install/install.py
@@ -11,38 +11,40 @@ from data.models.node_data import NodeData
 from data.models.node_type import NodeType
 
 
-async def execute_installation(node: Node, message: Message, callback_function):
-    await log_data_info(f"Execute installation for node {node.id}", message, callback_function)
+async def execute_installation(node: Node, message: Message):
+    strings_message = [f"Execute installation for node {node.id}"]
+    await message.answer('\n'.join(strings_message))
+
     node_type: NodeType = NodeType.get(NodeType.id == node.type)
     query = (InstallOperation
              .select()
              .where(InstallOperation.install_configuration == node_type.install_configuration)
              .order_by(InstallOperation.index))
     for operation in query:
-        await log_data_info(f"Execute operation {operation.file_path} for node {node.id}", message, callback_function)
+        await log_data_info(f"Execute operation {operation.file_path} for node {node.id}", message, strings_message)
         args = [] if operation.args == '' else operation.args.split(';')
         args_string = []
         for arg in args:
             node_data: NodeData = NodeData.get_or_none(NodeData.node_id == node.id,
                                                        NodeData.name == arg)
             if node_data is None:
-                await log_data_error(f"Arguments {arg} not found for node {node.id}", message, callback_function)
+                await log_data_error(f"Arguments {arg} not found for node {node.id}", message, strings_message)
                 return
             else:
                 args_string.append(node_data.data)
 
         script_file_path = config.INSTALL_SCRIPT_PATH + operation.file_path
-        await log_data_info(f"Execute script {script_file_path}", message, callback_function)
+        await log_data_info(f"Execute script {script_file_path}", message, strings_message)
 
         if operation.type == 0:
             await get_operation(node, script_file_path, args_string, message)
         elif operation.type == 1:
             await set_operation(node, script_file_path, args_string, message)
         else:
-            await log_data_error("Operation type not found", message, callback_function)
+            await log_data_error("Operation type not found", message, strings_message)
             return
 
-    await log_data_info(f"Complete execute installation for node {node.id}", message, callback_function)
+    await log_data_info(f"Complete execute installation for node {node.id}", message, strings_message)
 
 
 async def get_operation(node, file_path, args, message: Message):
@@ -91,17 +93,19 @@ async def set_operation(node, file_path, args, message: Message):
         await message.answer(f"Error operation {file_path} for {node.id}")
 
 
-async def log_data_info(text: str, message: Message, callback_function):
+async def log_data_info(text: str, message: Message, strings_message: []):
     logging.info(text)
-    #await log_data_callback(text, message, callback_function)
+    strings_message.append(text)
+    await message.edit_text('\n'.join(strings_message))
 
 
-async def log_data_error(text: str, message: Message, callback_function):
+async def log_data_error(text: str, message: Message, strings_message: []):
     logging.error(text)
-    #await log_data_callback(text, message, callback_function)
+    strings_message.append(text)
+    await message.edit_text('\n'.join(strings_message))
 
 
-async def log_data_callback(text: str, message: Message, callback_function):
-    if callback_function is None:
-        return
-    await callback_function(text, message)
+# async def log_data_callback(text: str, message: Message, callback_function):
+#     if callback_function is None:
+#         return
+#     await callback_function(text, message)
